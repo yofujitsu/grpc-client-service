bd52584c85965099e4a17723422ba9e408039543
yofujitsu <yofujitsuuu@gmail.com>
2024-08-24 19:29:22 +0300

    first commit

---
+++ src/main/java/org/startmakers/grpcserverservice/GrpcServerServiceApplication.java
@@ -0,0 +1,13 @@
+package org.startmakers.grpcserverservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcServerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrpcServerServiceApplication.class, args);
+    }
+
+}
